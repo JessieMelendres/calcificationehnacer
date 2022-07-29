e4aca6d3d52d949da80374197fef52ab1a5536cb
JessieMelendres <68960113+JessieMelendres@users.noreply.github.com>
2022-07-29 10:36:28 +0800

    Update views.py

--- main/views.py
+++ main/views.py
@@ -2,16 +2,4 @@ from django.shortcuts import render
 from .models import Files
 
 def index(response):
-    if response.method =='POST':
-        files=response.FILES.getlist('files')
-        file_list=[]
-        for file in files:
-            new_file=Files(
-                file=file
-            )
-            new_file.save()
-            file_list.append(new_file.file.url)
-
-        return render(response, "main/index.html", {'new_urls':file_list})
-    else:
-        return render(response, 'main/index.html')
+    return render(response, 'main/result.html')
